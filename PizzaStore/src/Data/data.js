c7d1408a11a8ff2cb2919788a0bef1ceb751927a
Johan Mattsson <mattsson.johan.93@gmail.com>
2024-04-08 13:44:35 +0200

    pizzakort

---
+++ PizzaStore/src/Data/data.js
@@ -0,0 +1,25 @@
+const pizzas = [
+	{
+	  id: 1,
+	  name: "Vegetariana-Marinara",
+	  info: "Classic pizza topped with tomato sauce, and vegetables",
+	  ingredients: ["Mozarella", "Tomatoes", "Olives", "Bell peppers", "Basil"],
+	  price: 100
+	},
+	{
+	  id: 2,
+	  name: "Pepperoni-Toni",
+	  info: "A white pizza with pepperoni goodness and a fine aged cheddar.",
+	  ingredients: ["Mozarella", "Aged Cheddar", "Pepperoni sausage"],
+	  price: 120
+	},
+	{
+	  id: 3,
+	  name: "Olive-Oliver",
+	  info: "Pizza loaded German olives and Swiss Tuna",
+	  ingredients: ["Mozarella", "Tomato sauce", "Mozzarella", "Olives", "Tuna"],
+	  price: 110
+	}
+  ];
+  
+export default pizzas
