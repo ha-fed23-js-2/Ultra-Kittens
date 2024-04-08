3c08b45cd6d8184ae3045e969ef6829fb65ef944
ziaalekozai <m_ziaalekozai.com>
2024-04-08 18:18:45 +0200

    fixat header för menu och mainsidan

--- PizzaStore/src/components/Header.jsx
+++ PizzaStore/src/components/Header.jsx
@@ -1,23 +1,69 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.png";
 import "../styles/Header.css";
 import { RiShoppingBasketLine } from "react-icons/ri";
-// import Quantity from "./Counter";
-
+import { MdOutlineArrowBack } from "react-icons/md";
 const Header = () => {
+  // State to manage the visibility of the headers
+  const [menuHeader, setMenuHeader] = useState(false);
+
+  // Toggle the menu visibility
+  const toggleMenu = () => {
+    setMenuHeader(!menuHeader);
+  };
+
+  // Reset the menu to show the main header and hide the menu header
+  const resetMenu = () => {
+    setMenuHeader(false);
+  };
+
   return (
     <header>
-      <div className="space"></div>
-      <div className="logo-container">
-        <NavLink to="/">
-          <img className="logo" src={Logo} alt="logo" />
-        </NavLink>
-
-        <h1>PizzakällarN</h1>
+      <div
+        className={
+          menuHeader ? "mainHeaderContainer hidden" : "mainHeaderContainer"
+        }
+      >
+        <div className="mainHeader">
+          <div className="space"></div>
+          <div className="logo-container">
+            <NavLink to="/">
+              <img className="logo" src={Logo} alt="logo" onClick={resetMenu} />
+            </NavLink>
+            <h1>PizzakällarN</h1>
+          </div>
+          <div className="shoppingCart">
+            <RiShoppingBasketLine className="shopping-Basket-icon" />
+            <div className="CountCartItemShow">1</div>
+          </div>
+        </div>
+        <nav>
+          <div>
+            <NavLink to="/menu">
+              <button className="menuButton" onClick={toggleMenu}>
+                Menu
+              </button>
+            </NavLink>
+          </div>
+        </nav>
       </div>
-      <div className="shoppingCart">
-        <RiShoppingBasketLine className="shopping-Basket-icon" />
-        <div className="CountCartItemShow">0</div>
+      <div className={menuHeader ? "menuHeader" : "menuHeader hidden"}>
+        <NavLink to="/">
+          <MdOutlineArrowBack className="backToMain-icon" onClick={resetMenu} />
+          {/* 
+            <img
+              className="menu-logo"
+              src={Logo}
+              alt="logo"
+              onClick={resetMenu}
+            /> */}
+        </NavLink>
+        <p>Menu</p>
+        <div className="shoppingCart">
+          <RiShoppingBasketLine className="shopping-Basket-icon" />
+          <div className="CountCartItemShow">1</div>
+        </div>
       </div>
     </header>
   );
