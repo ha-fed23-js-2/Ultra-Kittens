f47ef55ccd474532f19a0c4b4391d966c4a7aebf
Johan Mattsson <mattsson.johan.93@gmail.com>
2024-04-10 09:06:45 +0200

    store

--- PizzaStore/src/Data/store.js
+++ PizzaStore/src/Data/store.js
@@ -1,6 +1,15 @@
-import { create } from 'zustand'
+import create from 'zustand';
 
-const useMyStore = create(set => ({
-    username: 'Nisse',
-    balance: 0
-}))
+export const useAuthStore = create((set) => ({
+    isAuthenticated: false,
+    username: '',
+    login: (username, password) => {
+        // Jämför användarens inmatade användarnamn och lösenord med fördefinierade värden
+        if (username === 'Admin' && password === 'mums') {
+            set({ isAuthenticated: true, username: username });
+        } else {
+            console.log('Fel användarnamn eller lösenord');
+        }
+    },
+    logout: () => set({ isAuthenticated: false, username: '' }),
+}));
