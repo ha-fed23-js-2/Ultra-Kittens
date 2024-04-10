c9bd9ba8f73a5b5016238ce995b91ed31b546010
Johan Mattsson <mattsson.johan.93@gmail.com>
2024-04-10 14:13:41 +0200

    fix

--- PizzaStore/src/components/Mainmenu.jsx
+++ PizzaStore/src/components/Mainmenu.jsx
@@ -1,15 +1,39 @@
+import { MdOutlineEdit } from "react-icons/md";
+import { FaTrashAlt } from "react-icons/fa";
 import React, { useState } from "react";
 import AddToCart from "./AddToCart";
-import { pizzas, addPizza } from "../Data/data.js";
+import { pizzas, addPizza, deletePizza } from "../Data/data.js"; 
 import "../styles/Mainmenu.css";
 import AddPizzaButton from "./AddPizzaButton.jsx";
 
 const Mainmenu = () => {
   const [menuItems, setMenuItems] = useState(pizzas);
+  const [editPizza, setEditPizza] = useState(null);
+  const [showAddPizzaButton, setShowAddPizzaButton] = useState(true); 
 
   const handleAddPizza = (newPizza) => {
-    setMenuItems([...menuItems, newPizza]); 
+    setMenuItems([...menuItems, newPizza]);
     addPizza(newPizza);
+  };
+
+  const handleEditPizza = (pizza) => {
+    setEditPizza(pizza);
+    setShowAddPizzaButton(false); 
+  };
+
+  const handleSaveEdit = (editedPizza) => {
+    const updatedMenuItems = menuItems.map((item) =>
+      item.id === editedPizza.id ? editedPizza : item
+    );
+    setMenuItems(updatedMenuItems);
+    setEditPizza(null);
+    setShowAddPizzaButton(true); 
+  };
+
+  const handleDeletePizza = (pizzaId) => {
+    const updatedMenuItems = menuItems.filter((item) => item.id !== pizzaId);
+    setMenuItems(updatedMenuItems);
+    deletePizza(pizzaId); 
   };
 
   return (
@@ -31,12 +55,27 @@ const Mainmenu = () => {
               </div>
               <div className="price-container">
                 <p>{pizza.price} kr</p>
+                <div>
+                  
+				<MdOutlineEdit className="trashCan"onClick={() => handleEditPizza(pizza)}/>
+				  
+                  
+				  <FaTrashAlt className="trashCan" onClick={() => handleDeletePizza(pizza.id)}/>
+                </div> 
               </div>
             </div>
           </div>
         ))}
       </div>
-      <AddPizzaButton onAddPizza={handleAddPizza} />
+      {showAddPizzaButton && ( 
+        <AddPizzaButton onAddPizza={handleAddPizza} />
+      )}
+      {editPizza && (
+        <AddPizzaButton
+          onAddPizza={handleSaveEdit}
+          initialPizzaData={editPizza}
+        />
+      )}
     </div>
   );
 };
